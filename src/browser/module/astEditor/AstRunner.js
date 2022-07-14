4cedd2ea43f78b3b37c08218267b6523ade8db6a
Nick Aliferov <x8core@gmail.com>
2022-07-14 21:43:31 +0300

    AstRunner day 2

--- src/browser/module/astEditor/AstRunner.js
+++ src/browser/module/astEditor/AstRunner.js
@@ -1,3 +1,5 @@
+import Id from "./nodes/id/Id.js";
+
 export default class AstRunner {
 
     createModuleImports(imports, spacesCount) {
@@ -14,17 +16,54 @@ export default class AstRunner {
         return js;
     }
 
-    createJavascriptFromList(body, spaceCounts) {
+    createJavascriptFromList(body, spaceCounts, addInitialSpace = true) {
+
+        let s = '';
+
+        let addSpace = addInitialSpace;
+        let sp = () => {
+            if (addSpace) {
+                addSpace = false;
+                return ''.padStart(spaceCounts, ' ');
+            } else {
+                return ''
+            }
+        }
+
         for (let i = 0; i < body.length; i++) {
 
+            const d = body[i];
+            if (d.t === 'Id') {
+                let key = '';
+                if (d.mode === 'let') key = 'let ';
+                if (d.mode === 'new') key = 'new ';
+                s += sp() + key + d.name;
+            }
+            else if (d.t === 'Op') s += sp() + ' ' + d.op + ' ';
+            else if (d.t === 'Literal') s += sp() + d.txt;
+            else if (d.t === 'NewLine') {
+                s += sp() + '\n';
+                addSpace = true;
+            } else if (d.t === 'SubId') {
+                const container = d.container;
+                s += '.' + this.createJavascriptFromList(container, spaceCounts, false);
+            } else if (d.t === 'Call') {
+              s += '()';
+            }
+            else {
+                console.log('Unknown astNode type', d);
+            }
         }
+
+        return s;
     }
 
     createJavascriptCodeForFile(AST) {
 
+        console.log(AST);
+
         const js = [];
         js.push('let x = {}');
-
         js.push("x['main.imports'] = async () => {");
 
         js.push(this.createModuleImports(AST.imports, 4));
@@ -32,7 +71,7 @@ export default class AstRunner {
         js.push("}");
         js.push("x['main'] = async () => {");
 
-        this.createJavascriptFromList(AST.body, 4);
+        js.push(this.createJavascriptFromList(AST.body, 4));
 
         js.push("}");
 
@@ -43,7 +82,9 @@ export default class AstRunner {
 
         const js = this.createJavascriptCodeForFile(AST);
 
-        console.log('build javascript AST from AST and execute it', node, AST);
+        //console.log('build javascript AST from AST and execute it', node, AST);
         console.log(js.join('\n'));
+
+        e('processRun', {js});
     }
 }
